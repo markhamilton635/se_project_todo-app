97f040114a4051a204a3b7d7391623abafc64e19
MARK HAMILTON <hamilton.mark37@gmail.com>
2025-01-14 20:49:19 -0800

    finalize close modal functionality with escape/mousedown outside of content

--- components/Popup.js
+++ components/Popup.js
@@ -2,20 +2,38 @@ class Popup {
   constructor({ popupSelector }) {
     this._popupElement = document.querySelector(popupSelector);
     this._popupCloseBtn = this._popupElement.querySelector(".popup__close");
+    this._handleEscapeClose = this._handleEscapeClose.bind(this);
   }
+
   open() {
     this._popupElement.classList.add("popup_visible");
+    document.addEventListener("keyup", this._handleEscapeClose);
   }
   close() {
     this._popupElement.classList.remove("popup_visible");
+    document.removeEventListener("keyup", this._handleEscapeClose);
   }
-
-  _handleEscapeClose() {}
 
   setEventListeners() {
     this._popupCloseBtn.addEventListener("click", () => {
       this.close();
     });
+
+    this._popupElement.addEventListener("mousedown", (evt) => {
+      debugger;
+      if (
+        evt.classList === "popup__close" ||
+        evt.target === this._popupElement
+      ) {
+        this.close();
+      }
+    });
+  }
+
+  _handleEscapeClose(evt) {
+    if (evt.key === "Escape") {
+      this.close();
+    }
   }
 }
 export default Popup;
